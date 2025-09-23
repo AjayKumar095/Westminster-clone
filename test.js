378539ab85c82b473f634ef74616d3f659fe3f1e
Ajay <jaykumar002kori@gmail.com>
2025-09-23 12:20:38 +0530

    commit

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-console.log('Hello');
+function ist() 
+  { console.log("Run 1");};
+
+function iind() {
+    setTimeout(() => {
+        console.log("Run 2, it will delay for 4 seconds");
+    }, 4000);
+}
+
+function ithree() {
+    console.log("Run 3");
+}
+
+ist();
+iind();
+ithree();
+
